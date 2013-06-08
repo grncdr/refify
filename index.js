deeda10132592ee68a08b53845a8b8e2b33f418d
Stephen Sugden <me@stephensugden.com>
2013-06-08 12:07:44 -0700

    remove traverse dependency

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 module.exports = refify
 
-var traverse = require('traverse');
-
 function refify(obj) {
   var objs = [];
   var paths = []
@@ -18,7 +16,7 @@ function refify(obj) {
     objs.push(it);
     paths.push(keyStack.slice())
     objStack.push(it)
-    var copy = Array.isArray(it) ? [] : {}
+    var copy = initCopy(it);
     for (var k in it) {
       keyStack.push(k);
       var v = it[k];
@@ -37,23 +35,39 @@ function refify(obj) {
 }
 
 refify.parse = function (it) {
-  if (typeof it !== 'object') it = JSON.parse(it)
-  if (typeof it !== 'object') return it;
+  if (typeof it !== 'object') it = JSON.parse(it);
 
-  var copy = traverse(it).clone();
-  var t = traverse(copy);
-  t.forEach(function () {
-    var current = this.node;
-    var objPath = parseRef(current);
-    while (objPath) {
-      current = t.get(objPath);
-      objPath = parseRef(current);
-    }
-    if (current !== this.node) {
-      this.update(current, true);
-    }
-  })
+  var keyStack = [];
+  var copy = initCopy(it);
+
+  walk(it);
+
   return copy;
+
+  function walk(obj) {
+    if (typeof obj !== 'object') {
+      set(copy, keyStack.slice(), obj);
+      return;
+    }
+    for (var k in obj) {
+      keyStack.push(k);
+      var current = obj[k];
+      var objPath = parseRef(current);
+      while (objPath) {
+        current = get(copy, objPath);
+        objPath = parseRef(current);
+      }
+      if (current === obj[k]) {
+        // We did *not* follow a reference
+        set(copy, keyStack.slice(), initCopy(current));
+        walk(current);
+      } else {
+        // We *did* follow a reference
+        set(copy, keyStack.slice(), current);
+      }
+      keyStack.pop();
+    }
+  }
 }
 
 refify.stringify = function (obj, replacer, spaces) {
@@ -65,4 +79,36 @@ function parseRef(value) {
   if (!value.$ref) return false;
   var path = value.$ref == '#/' ? [] : value.$ref.split('/').slice(1);
   return path
+}
+
+function get(obj, path) {
+  if (!path.length) return obj;
+  if (typeof obj !== 'object') return;
+  var next = obj[path.shift()];
+  return get(next, path);
+}
+
+refify.set = set;
+function set(obj, path, value) {
+  if (path.length === 0) throw new Error("Cannot replace root object");
+  var key = path.shift();
+  if (!path.length) {
+    obj[key] = value;
+    return;
+  }
+  switch (typeof obj[key]) {
+  case 'undefined':
+    obj[key] = isNaN(parseInt(key, 10)) ? {} : [];
+    break;
+  case 'object':
+    break;
+  default:
+    throw new Error("Tried to set property " + key + " of non-object " + obj[key]);
+  }
+  set(obj[key], path, value);
+}
+
+function initCopy(obj) {
+  if (typeof obj !== 'object') return obj;
+  return Array.isArray(obj) ? [] : {}
 }
